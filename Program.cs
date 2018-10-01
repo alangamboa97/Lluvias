e8200e446e187ac483522492068b0898e768dc33
Alan Gamboa <alangam97@gmail.com>
2018-10-01 07:45:53 -0500

    ya funciona esta wea

--- Program.cs
+++ Program.cs
@@ -6,136 +6,65 @@ namespace LLuvias
     {
         static void Main(string[] args)
         {
-           int filas, columnas, random;
-           int suma =0, p=0;
            
-           String[] estados = {"Sonora","Aguascalientes","Oaxaca","Colima","Chihuahua","San Luis","Durango","Veracruz","Yucatan","Tabasco"};
-           
-           
-           int sonora =0;
-           int aguas =0;
-           int oaxaca =0;
-           int colima =0;
-           int chihuahua =0;
-           int sanluis =0;
-           int durango =0;
-           int veracruz =0;
-           int yucatan =0;
-           int tabasco=0;
-           int contador;
-           
+            Random random = new Random();
+            String x = "",y = "",z = "";
+            int precipitacion;
+            int mayorlluvia = 0;
+            int mayorAguas = 0;
+            int menor = 250;
 
-           Random r = new Random();
-           int[,] lluvias = new int[10,12];
-           
+            int[,] lluvias = new int[10, 12];
 
-           for(filas=0;filas<10;filas++){ //llena las filas
-               
-               for(columnas=0; columnas<12; columnas++){ //llena las columnas
+            String[] estados = new String[10] { "Sonora:\t", "Aguascalientes:", "Oaxaca:\t", "Colima:\t"
+                , "Chihuahua","San Luis Potosí:","Durango:","Veracruz:","Yucatan\t","Durango\t" };
+
+            String[] meses = new String[12] {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
                 
-                   random = r.Next(1,101);
-                   lluvias[filas,columnas] = random;
-                   
-                   Console.Write("{0}\t",random);
-                  
+            int[] promedio = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-                   
-                   }
-                    
-                     Console.WriteLine();
-            }       
-           
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(estados[i] + "\t");
+                for (int j = 0; j < 12; j++)
+                {
+                    precipitacion = random.Next(0, 251);// genera los va
+                    lluvias[i, j] = precipitacion;
 
-            for(filas =0; filas <=0; filas++){
-                for (columnas =0; columnas<12; columnas ++){
-                    sonora = sonora + lluvias[filas,columnas];
+                    Console.Write(lluvias[i, j] + "\t");
+                    if (lluvias[1, j] > mayorAguas)
+                    {
+                        mayorAguas = lluvias[1, j];
+                        z = meses[j];
+                    }
+                    promedio[i] = promedio[i] + lluvias[i, j];
                 }
-
-            }
-            for(filas=1; filas<=1; filas++){
-                for (columnas =0; columnas<12; columnas++){
-                    aguas = aguas+ lluvias[filas,columnas];
-                }
-            }
-            for(filas =2; filas<=2; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    oaxaca = oaxaca + lluvias[filas,columnas];
-                }
-
-            }
-            for(filas =3; filas<=3; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    colima = colima + lluvias[filas,columnas];
-                }
-
-            }
-            for(filas =4; filas<=4; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    chihuahua = chihuahua + lluvias[filas,columnas];
-                }
-
-            }for(filas =5; filas<=5; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    sanluis = sanluis + lluvias[filas,columnas];
-                }
-
-            }for(filas =6; filas<=6; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    durango = durango + lluvias[filas,columnas];
-                }
-
-            }for(filas =7; filas<=7; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    veracruz = veracruz + lluvias[filas,columnas];
-                }
-
-            }for(filas =8; filas<=8; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    yucatan = yucatan + lluvias[filas,columnas];
-                }
-
-            }for(filas =9; filas<=9; filas++){
-                for(columnas=0; columnas<12; columnas++){
-                    tabasco = tabasco + lluvias[filas,columnas];
-                }
-            }
+                promedio[i] = promedio[i] / 12; //calcula el promedio de lluvias por cada estado
                 
-            int[] lluviaestados = {sonora,colima,veracruz,chihuahua,durango,sanluis,tabasco,aguas,oaxaca};
-           
-          
-        for(filas =0; filas <9; filas++){
-            for(columnas=0; columnas <=0; columnas++){
-                Console.Write("\t{0}", lluviaestados[filas]);
-                Console.Write("\t{0}",estados[filas]);
-                    
-                
+                if (promedio[i] > mayorlluvia) { //devuelve el nombre del estado con mas lluvias
+                    mayorlluvia = promedio[i];
+                    x = estados[i];
+                }
+                if (promedio[i] < menor) { //devuelve el nombre del estado con menor lluvias
+                    menor = promedio[i];
+                    y = estados[i];
+                }
+                Console.WriteLine(promedio[i]);
             }
-            Console.WriteLine();
-
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Estado con mas lluvias: "+x+ "con :"+mayorlluvia);
+            Console.WriteLine("Estado con menor lluvias: " + y + "con :" + menor);
+            Console.WriteLine("El mes con mas lluvias en Aguascalientes fue " + z + " con :" + mayorAguas);
+            
             
         }
-      
-        int [] lluviasordenadas = new int[10];
-        
-         //metodo de ordenamiento burbuja
-        
-        int t;
-            for (int a = 1; a < lluviaestados.Length; a++){
-                for (int b = lluviaestados.Length - 1; b >= a; b--)
-                {
-                    if (lluviaestados[b - 1] > lluviaestados[b])
-                    {
-                        t = lluviaestados[b - 1];
-                        lluviaestados[b - 1] = lluviaestados[b];
-                        lluviaestados[b] = t;
-                    }
-                }
-            }
             
-            //ordenamiento burbuja para aguascalientes
-           
-        Console.WriteLine("El estado con menor lluvias es :{0}",lluviaestados[0]);
-        Console.WriteLine("El estado con mayor lluvia es : {0}", lluviaestados[8]);
+          
+    }
+}
+
+
 
          
             
@@ -148,9 +77,9 @@ namespace LLuvias
 
 
 
-     }
- }
-}
+     
+ 
+
 
 
 
